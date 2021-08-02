24d96899a2516f426f06d919cdb9b03998602d48
Lucian I. Last <li@last.nl>
2021-08-02 16:32:50 +0200

    Started on website

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+	purge: [],
+	darkMode: false,
+	theme: {
+		extend: {
+			colors: {
+				primary: {
+					100: "#f0f7f7",
+					200: "#d8e8e4",
+					300: "#002626",
+				}
+			}
+		},  
+	},
+	variants: {},
+	plugins: [],
+}
+
