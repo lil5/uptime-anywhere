65fc20c2ee2f5595cf388bb3c0f74d2c20595a91
Lucian I. Last <li@last.nl>
2021-08-05 09:08:20 +0200

    correct chart config

--- website/src/html/chart.ts
+++ website/src/html/chart.ts
@@ -3,6 +3,7 @@ import {
 	ChartConfiguration,
 	ChartDataset,
 	registerables,
+	ScriptableLineSegmentContext,
 } from "chart.js"
 
 import { ERROR_GET_ELEMENT_BY_ID } from "../errors"
@@ -10,72 +11,80 @@ import { CalculatedDataSite, CalculatedDataItem, DataSite } from "../types"
 
 Chart.register(...registerables)
 
+const WARNING = "#F87171"
+const DEFAULT = "#89e0cf"
+
 export function generateChart(d: CalculatedDataSite) {
-	console.log(d)
 	const el = document.getElementById(`canvas-live-status-${d.name}`)
 	if (!el) throw Error(ERROR_GET_ELEMENT_BY_ID(`canvas-live-status-${d.name}`))
 
-	const isUp = false
+	const data = [
+		{ x: 30, y: 20, date: "2021-05-02", status: "up" },
+		{ x: 40, y: 100, date: "2021-05-01", status: "up" },
+		{ x: 90, y: 120, date: "2021-05-01", status: "down" },
+		{ x: 119, y: 80, date: "2021-05-08", status: "up" },
+		{ x: 120, y: 80, date: "2021-05-08", status: "up" },
+	]
 
-	// similar to
+	function statusColor(ctx: ScriptableLineSegmentContext): string {
+		return data[ctx.p0DataIndex].status === "up" ? DEFAULT : WARNING
+	}
+
 	const config: ChartConfiguration<"scatter", any[]> = {
 		type: "scatter",
 		data: {
 			datasets: [
 				{
 					fill: true,
-					data: [
-						{ x: 30, y: 20 },
-						{ x: 40, y: 100 },
-						{ x: 90, y: 120 },
-						{ x: 120, y: 80 },
-					],
-					backgroundColor: isUp ? "#89e0cf" : "#F87171",
-					borderWidth: 2,
+					data,
+					borderWidth: -10,
+					segment: {
+						backgroundColor: statusColor,
+					},
 				},
 			],
-			labels: ["1", "2", "3", "4"],
 		},
 		options: {
 			animation: false,
-			showLine: true,
 			plugins: {
 				legend: { display: false },
-				filler: { propagate: false },
-				tooltip: { enabled: false },
+				tooltip: {
+					enabled: true,
+					displayColors: false,
+					callbacks: {
+						label: (ctx) => `${data[ctx.dataIndex].y} ms`,
+						afterLabel: (ctx) => `${data[ctx.dataIndex].date}`,
+					},
+				},
 			},
 			elements: {
-				point: {
-					radius: 0,
-					hitRadius: 0,
-				},
 				line: {
-					tension: 0,
 					stepped: true,
+					borderWidth: 0,
 				},
 			},
+			interaction: {
+				mode: "nearest",
+				intersect: false,
+			},
 			responsive: true,
+			maintainAspectRatio: true,
 			aspectRatio: 160 / 107,
 			scales: {
 				y: {
-					display: false,
-					grid: { display: false },
 					beginAtZero: true,
+					display: false,
 				},
 				yAxis: {
-					ticks: { display: false },
 					display: false,
-					grid: { display: false },
 				},
 				x: {
 					min: 30,
 					max: 120,
 					display: false,
-					grid: { display: false },
 				},
 				xAxis: {
 					display: false,
-					grid: { display: false },
 				},
 			},
 		},
