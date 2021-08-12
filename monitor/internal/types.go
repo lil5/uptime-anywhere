074148316273b1031fede9106831fd95b5bb4a1a
Lucian I. Last <li@last.nl>
2021-08-12 16:09:40 +0200

    First test

--- monitor/internal/types.go
+++ monitor/internal/types.go
@@ -7,16 +7,23 @@ const (
 )
 
 type ConfigSite struct {
-	Name string `required:"true" json:"name"`
-	Url  string `required:"true" json:"url"`
+	Name string `required:"true" json:"name" yaml:"name"`
+	Url  string `required:"true" json:"url" yaml:"url"`
+}
+
+type ConfigNotifications struct {
+	Smtp bool `default:"false" yaml:"smtp"`
 }
 
 type Config struct {
-	Host   string       `required:"true" json:"host"`
-	Owner  string       `required:"true" json:"owner"`
-	Repo   string       `required:"true" json:"repo"`
-	Branch string       `required:"true" json:"branch"`
-	Sites  []ConfigSite `required:"true" json:"sites"`
+	Host          string              `required:"true" json:"host" yaml:"host"`
+	Owner         string              `required:"true" json:"owner" yaml:"owner"`
+	Repo          string              `required:"true" json:"repo" yaml:"repo"`
+	Branch        string              `required:"true" json:"branch" yaml:"branch"`
+	Sites         []ConfigSite        `required:"true" json:"sites" yaml:"sites"`
+	Notifications ConfigNotifications `json:"notifications" yaml:"notifications"`
+	WebsiteUrl    string              `json:"website_url" yaml:"website_url"`
+	GithostUrl    string              `json:"githost_url" yaml:"githost_url"`
 }
 
 type ResultSite struct {
@@ -25,4 +32,20 @@ type ResultSite struct {
 	ResponseTime int
 	HttpCode     int
 	Timestamp    string
+}
+
+type SiteStatusChange struct {
+	Name          string
+	ChangedToUp   bool
+	ChangedToDown bool
+	LaterThanN    bool
+	First         bool
+}
+
+func (ssc *SiteStatusChange) ShouldWrite() bool {
+	return ssc.ChangedToDown || ssc.ChangedToUp || ssc.LaterThanN || ssc.First
+}
+
+func (ssc *SiteStatusChange) ShouldNotify() bool {
+	return ssc.ChangedToDown || ssc.ChangedToUp || ssc.First
 }
--- monitor/main.go
+++ monitor/main.go
@@ -9,27 +9,40 @@ import (
 
 func main() {
 	config := internal.LoadConfig()
+	envIsDev := os.Getenv("DEV") == "true"
 
 	result := internal.CallAll(config.Sites)
 
-	hasWritten, err := internal.WriteAll(result)
+	hasAnyWritten, sscList, err := internal.WriteAll(result)
 	if err != nil {
 		fmt.Print(err)
 		os.Exit(1)
 	}
-	if !hasWritten {
+	if !hasAnyWritten {
 		os.Exit(0)
-	} else {
-		err = internal.WriteConfigJSON(config)
+	}
+
+	message, err := internal.BuildMessage(result, *sscList)
+	if err != nil {
+		fmt.Print(err)
+		os.Exit(1)
+	}
+
+	err = internal.WriteConfigJSON(config)
+	if err != nil {
+		fmt.Print(err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("env is dev %t", envIsDev)
+
+	if !envIsDev {
+		err = internal.RunGit(message)
 		if err != nil {
 			fmt.Print(err)
 			os.Exit(1)
 		}
 	}
 
-	// err = internal.RunGit(result)
-	// if err != nil {
-	// 	fmt.Print(err)
-	// 	os.Exit(1)
-	// }
+	internal.RunNotifications(config, message)
 }
